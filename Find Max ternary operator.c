7ac55755554db8b3b16405d8125d4bcc2e1955b3
nzarifi <49527718+nzarifi@users.noreply.github.com>
2021-10-20 13:59:50 -0400

    Add files via upload

---
+++ Find Max ternary operator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int max1=(a>b)?a:b;
+    int max2 = (c>d)?c:d;
+    return (max1>max2)?max1:max2;
+}
+
+
+int main() {
+    int a, b, c, d,anss;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+
+    printf("%d", ans);
+    
+    return 0;
+}
