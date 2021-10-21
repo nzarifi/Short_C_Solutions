73a05c5c257d1eecc54cff106a53ca050bfea9ec
nzarifi <49527718+nzarifi@users.noreply.github.com>
2021-10-21 14:41:23 -0400

    Add files via upload

---
+++ pattern using loop.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+
+int main()
+{
+
+    int n,i,j,v,arr[4];
+    scanf("%d", &n);
+    for(i=1;i<2*n;++i)
+    {
+        for(j=1;j<2*n;++j)
+        {
+            int a =(i<j ?i:j);
+            int b  = ((2*n-i)<a ?(n*2-i):a) ;
+            v = ((2*n-j)<b ?(n*2-j):b );
+            printf("%d ",(n-v+1));
+        }printf("\n");
+    }
+  	// Complete the code to print the pattern.
+    return 0;
+}
